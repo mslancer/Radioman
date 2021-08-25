5ca707566714237ffd7184d1cbbcbb2c9772366c
mslancer <85640422+mslancer@users.noreply.github.com>
2021-08-25 19:45:18 +0500

    Update RadioTest.java

--- src/test/java/ru/netology/RadioTest.java
+++ src/test/java/ru/netology/RadioTest.java
@@ -9,12 +9,10 @@ public class RadioTest {
 
     // Тесты Радио
 
-
     @Test
     public void shouldMaxRadioStation() {
-
         radio.setCurrentRadioStation(radio.getMaxRadioStation());
-        assertEquals(radio.getCurrentRadioStation(), radio.getCurrentRadioStation() );
+        assertEquals(radio.getCurrentRadioStation(), radio.getCurrentRadioStation());
 
     }
 
@@ -30,7 +28,7 @@ public class RadioTest {
         radio.setCurrentRadioStation(7);
         radio.setCurrentRadioStation(8);
         radio.setCurrentRadioStation(9);
-        assertEquals(radio.getCurrentRadioStation(), radio.getCurrentRadioStation() );
+        assertEquals(radio.getCurrentRadioStation(), radio.getCurrentRadioStation());
 
     }
 
@@ -38,7 +36,7 @@ public class RadioTest {
     public void shouldMaxRadioStationOverLimit() {
 
         radio.setCurrentRadioStation(radio.getMaxRadioStation() + 1);
-        assertEquals(radio.getCurrentRadioStation(), radio.getCurrentRadioStation() );
+        assertEquals(radio.getCurrentRadioStation(), radio.getCurrentRadioStation());
 
     }
 
@@ -46,7 +44,7 @@ public class RadioTest {
     public void shouldMaxRadioStationUnderLimit() {
 
         radio.setCurrentRadioStation(radio.getMinRadioStation() - 1);
-        assertEquals(radio.getCurrentRadioStation(), radio.getCurrentRadioStation() );
+        assertEquals(radio.getCurrentRadioStation(), radio.getCurrentRadioStation());
 
     }
 
@@ -54,7 +52,7 @@ public class RadioTest {
     @Test
     public void shouldMinRadioStation() {
         radio.setCurrentRadioStation(radio.getMinRadioStation());
-        assertEquals(radio.getCurrentRadioStation(), radio.getCurrentRadioStation() );
+        assertEquals(radio.getCurrentRadioStation(), radio.getCurrentRadioStation());
 
     }
 
@@ -63,7 +61,7 @@ public class RadioTest {
 
         radio.setCurrentRadioStation(radio.getMaxRadioStation());
         radio.increaseRadio();
-        assertEquals(radio.getCurrentRadioStation(), radio.getCurrentRadioStation() );
+        assertEquals(radio.getCurrentRadioStation(), radio.getCurrentRadioStation());
 
     }
 
@@ -72,17 +70,17 @@ public class RadioTest {
 
         radio.setCurrentRadioStation(radio.getMinRadioStation());
         radio.decreaseRadio();
-        assertEquals(radio.getCurrentRadioStation(), radio.getCurrentRadioStation() );
+        assertEquals(radio.getCurrentRadioStation(), radio.getCurrentRadioStation());
 
     }
 
     @Test
     public void shouldIncreases2xFromMaxStation() {
 
-        radio.setCurrentRadioStation(radio.getMaxRadioStation() );
+        radio.setCurrentRadioStation(radio.getMaxRadioStation());
         radio.increaseRadio();
         radio.increaseRadio();
-        assertEquals(radio.getCurrentRadioStation(), radio.getCurrentRadioStation() );
+        assertEquals(radio.getCurrentRadioStation(), radio.getCurrentRadioStation());
 
     }
 
@@ -92,7 +90,7 @@ public class RadioTest {
         radio.setCurrentRadioStation(5);
         radio.increaseRadio();
         radio.decreaseRadio();
-        assertEquals(radio.getCurrentRadioStation(), radio.getCurrentRadioStation() );
+        assertEquals(radio.getCurrentRadioStation(), radio.getCurrentRadioStation());
 
     }
 
@@ -102,14 +100,14 @@ public class RadioTest {
     public void shouldMaxVolume() {
 
         radio.setCurrentVolume(radio.getMaxVolume());
-        assertEquals(radio.getCurrentVolume(), radio.getCurrentVolume() );
+        assertEquals(radio.getCurrentVolume(), radio.getCurrentVolume());
 
     }
 
     @Test
     public void shouldMinVolume() {
         radio.setCurrentVolume(radio.getMinVolume());
-        assertEquals(radio.getCurrentVolume(), radio.getCurrentVolume() );
+        assertEquals(radio.getCurrentVolume(), radio.getCurrentVolume());
 
     }
 
@@ -117,14 +115,14 @@ public class RadioTest {
     public void shouldMaxVolumeOverLimit() {
 
         radio.setCurrentVolume(radio.getMaxVolume() + 1);
-        assertEquals(radio.getCurrentVolume(), radio.getCurrentVolume() );
+        assertEquals(radio.getCurrentVolume(), radio.getCurrentVolume());
 
     }
 
     @Test
     public void shouldMinVolumeUnderLimit() {
         radio.setCurrentVolume(radio.getMinVolume() - 1);
-        assertEquals(radio.getCurrentVolume(), radio.getCurrentVolume() );
+        assertEquals(radio.getCurrentVolume(), radio.getCurrentVolume());
 
     }
 
@@ -133,15 +131,16 @@ public class RadioTest {
 
         radio.setCurrentVolume(radio.getMaxVolume());
         radio.increaseVolume();
-        assertEquals(radio.getCurrentVolume(), radio.getCurrentVolume() );
+        assertEquals(radio.getCurrentVolume(), radio.getCurrentVolume());
 
     }
+
     @Test
     public void shouldMinAndDecreaseVolume() {
 
         radio.setCurrentVolume(radio.getMinVolume());
         radio.decreaseVolume();
-        assertEquals(radio.getCurrentVolume(), radio.getCurrentVolume() );
+        assertEquals(radio.getCurrentVolume(), radio.getCurrentVolume());
 
     }
 
@@ -152,7 +151,7 @@ public class RadioTest {
         radio.increaseVolume();
         radio.decreaseVolume();
         radio.increaseVolume();
-        assertEquals(radio.getCurrentVolume(), radio.getCurrentVolume() );
+        assertEquals(radio.getCurrentVolume(), radio.getCurrentVolume());
 
     }
 
@@ -162,7 +161,7 @@ public class RadioTest {
         radio.setCurrentVolume(9);
         radio.increaseVolume();
         radio.decreaseVolume();
-        assertEquals(radio.getCurrentVolume(), radio.getCurrentVolume() );
+        assertEquals(radio.getCurrentVolume(), radio.getCurrentVolume());
 
     }
 
@@ -172,8 +171,9 @@ public class RadioTest {
         radio.setCurrentVolume(1);
         radio.decreaseVolume();
         radio.decreaseVolume();
-        assertEquals(radio.getCurrentVolume(), radio.getCurrentVolume() );
+        assertEquals(radio.getCurrentVolume(), radio.getCurrentVolume());
 
     }
 
 }
+
